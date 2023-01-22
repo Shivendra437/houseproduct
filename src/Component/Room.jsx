41a03a6d5e01582b0d0f71d0d7279af532688c91
Shivendra437 <104500591+Shivendra437@users.noreply.github.com>
2023-01-22 14:28:43 +0530

    1 file modified, 1 added, 1 deleted

--- src/Component/Room.jsx
+++ src/Component/Room.jsx
@@ -1,17 +1,14 @@
 import React from "react";
-import room from "./room.css";
-import Photo from "./product-1.jpeg"
 export default function Room(props) {
   return (
-    <div className="roomsline">
-      <div className="rooms">
+    <div>
+      <div>
         <img className="images" src={props.images} alt="room" />
       </div>
       <div> Title:{props.names}</div>
       <div>Type:{props.type}</div>
       <div>Rating:{props.rating}</div>
       <div>Price:{props.price}</div>
-      <img src={Photo} alt="#"/>
     </div>
   );
 }
