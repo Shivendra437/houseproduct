476f5ec6d39b17db9463a83f7df9e0743ea51bc5
Shivendra437 <104500591+Shivendra437@users.noreply.github.com>
2023-01-22 13:18:52 +0530

    1 file modified

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -11,7 +11,7 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import HouseIcon from '@mui/icons-material/House';
 
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
@@ -39,7 +39,7 @@ export default function Navbar() {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <HouseIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
           <Typography
             variant="h6"
             noWrap
@@ -94,7 +94,8 @@ export default function Navbar() {
               ))}
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          {/* icon */}
+          <HouseIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />    
           <Typography
             variant="h5"
             noWrap
