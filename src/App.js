93bc1ad3b3c704c2af62cc3db1ab268424c5a7f2
Shivendra437 <104500591+Shivendra437@users.noreply.github.com>
2023-01-22 12:28:14 +0530

    4 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import "./style.css";
-
+import Navbar from "./Component/Navbar"
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+<Navbar/>
     </div>
   );
 }
